f81dbaadd99d868445a4c8acead57836142dc7f7
Stephen <stphnchristian@gmail.com>
2015-02-19 10:16:05 -0500

    -Modified make_eclipse_project.py to create project directory if it does not exist. -Fixed C++11 indexing with new Eclipse project files.

--- scripts/make_eclipse_project.py
+++ scripts/make_eclipse_project.py
@@ -4,35 +4,36 @@ import subprocess
 import sys
 
 def main():
-    #We use argparse to get the directory, as well as display help/usage information.
+    # We use argparse to get the directory, as well as display help/usage information.
     parser = argparse.ArgumentParser(
         description='Generates Eclipse project files in the specified directory.')
     parser.add_argument(
         'directory', metavar='<directory>', help='path to the new project directory')
+    
+    # We then make the directory 
     bin_dir = parser.parse_args().directory
-
-    if not os.path.isdir(bin_dir):
-        print '\t' + bin_dir + ' is not a valid directory.'
-        return
     bin_dir = os.path.abspath(bin_dir)
+    os.makedirs(bin_dir)
 
-    #Get the path to ROS's mk package
+    # Get the path to ROS's mk package.
     mk_dir = subprocess.check_output(['rospack', 'find', 'mk']).strip()
     if not os.path.isdir(mk_dir):
         print "Could not find ROS's 'mk' package."
         return
 
-    #Get the top level directory of the source files
+    # Get the top level directory of the source files.
     script_dir = os.path.dirname(os.path.realpath(__file__))
     src_dir = os.path.abspath(os.path.join(script_dir, os.path.pardir))
 
     print 'Project Directory: ' + bin_dir
     print 'Source Directory: ' + src_dir
 
-    #Run CMake in the project directory using the Eclipse generator
-    subprocess.Popen(['cmake', '-G', 'Eclipse CDT4 - Unix Makefiles', src_dir], cwd = bin_dir).wait()
+    # Run CMake in the project directory using the Eclipse generator.
+    # We use CMAKE_CXX_COMPILER_ARG1 to enable C++11 in Eclipse.
+    subprocess.Popen(['cmake', '-G', 'Eclipse CDT4 - Unix Makefiles', 
+        '-DCMAKE_CXX_COMPILER_ARG1=-std=c++11', src_dir], cwd = bin_dir).wait()
 
-    #Run ROS's awk script to pass the current shell environment into the make process in Eclipse
+    #Run ROS's awk script to pass the current shell environment into the make process in Eclipse.
     xml_string = subprocess.check_output(['awk', '-f', os.path.join(mk_dir, 'eclipse.awk'), os.path.join(bin_dir, '.project')])
     with open(os.path.join(bin_dir, '.project'), 'w') as f:
         f.write(xml_string)
