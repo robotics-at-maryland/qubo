2198be1bc16acc18ef9b5d601214190e00fd4506
Greg Harris <gharris1727@gmail.com>
2015-10-08 20:24:58 -0400

    Began refactoring sample code

---
+++ packages/qubo_ahrs/sample/read.h
@@ -0,0 +1,8 @@
+/*
+ * read.h
+ * Sample program for reading data from the AHRS sensor over usb.
+ * Derived from the sample code from pnicorp.com
+ */
+#include "ahrs.h"
+SInt32 Setup_Sentral();
+SInt32 ResetSentral(UInt8 command);
