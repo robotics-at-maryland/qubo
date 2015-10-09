8c4a74d480aa4cea0479657ced20073c1cfccf66
Jeremy Weed <jweed262@gmail.com>
2015-10-08 20:04:56 -0400

    started work on the vehicle layer

---
+++ packages/qubo_vehicle_layer/src/device.cpp
@@ -0,0 +1,16 @@
+#include "device.h"
+
+//default abstract class that all devices in the vehicle_layer
+//should inherit from
+//not actually written yet
+virtual class Device{
+public:
+		/*
+		data
+		publish
+		subscribe
+		push
+		*/
+private:
+
+}
