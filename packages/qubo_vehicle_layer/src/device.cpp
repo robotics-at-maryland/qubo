473348af939a65750847039624113006a2c0563c
Jeremy Weed <jweed262@gmail.com>
2015-10-13 20:11:51 -0400

    figuring out how ros works still

--- packages/qubo_vehicle_layer/src/device.cpp
+++ packages/qubo_vehicle_layer/src/device.cpp
@@ -1,5 +1,6 @@
 #include "device.h"
 
+
 //default abstract class that all devices in the vehicle_layer
 //should inherit from
 //not actually written yet
@@ -11,6 +12,15 @@ public:
 		subscribe
 		push
 		*/
+
+		void publish();
+		void subscribe();
+
+	int main(int arga, char **argv){
+
+		ros::init(argc, argv, "virtual_device");
+		ros::NodeHandle n;
+	}
 private:
 
 }
