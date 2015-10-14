80e7f1923c8972e7cee59031ac7bcad0912109f2
Greg Harris <gharris1727@gmail.com>
2015-10-13 20:17:15 -0400

    Further testing, not quite working yet.

---
+++ packages/qubo_ahrs/sample/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "imuapi.h"
+
+int main(){
+   int imu = openIMU("/dev/ttyUSB0");
+   RawIMUData data; 
+   printf("Connected with file descriptor %d",imu);
+   while (1){
+      printf("READING\n");
+      readIMUData(imu,&data);
+      printf("ID: %d, Timer: %d", data.messageID, data.sampleTimer);
+      printf("Gyro rad/s %f/%f/%f", data.gyroX, data.gyroY, data.gyroZ);
+      printf("Accel Gs %f/%f/%f", data.accelX, data.accelY, data.accelZ);
+      printf("Mag gauss %f/%f/%f", data.magX, data.magY, data.magZ);
+      printf("Temp rad/s %f/%f/%f", data.tempX, data.tempY, data.tempZ);
+      printf("Checksum: %d\n",data.checksumValid);
+   }
+   close(imu);
+   return 0;
+}
