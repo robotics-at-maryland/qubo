dfd5bde87f769439cdd01af2addb2c9acad59971
John Kastner <kastner@terpmail.umd.edu>
2015-10-27 19:01:45 -0400

    Previouse commit was incomplete

--- packages/qubo_sim/src/temperature_sensor_sim.cpp
+++ packages/qubo_sim/src/temperature_sensor_sim.cpp
@@ -1,21 +1,52 @@
 #include "ros/ros.h"
 #include "ram_msgs/Sim_Temperature.h"
 
+#include <random>
+
+#define DEFAULT_MEAN_TEMP 23.0
+
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "simulated_temperature_sensor");
-  
-  ros::NodeHandle n;
-  ros::Publisher temperature_sensor = n.advertise<ram_msgs::Sim_Temperature>("/qubo/temperature", 100);
+    ros::init(argc, argv, "simulated_temperature_sensor");
+    
+    ros::NodeHandle n;
+    ros::Publisher temperature_sensor = n.advertise<ram_msgs::Sim_Temperature>("/qubo/temperature", 100);
 
-  ros::Rate loop_rate(100);
+    ros::Rate loop_rate(100);
 
-  while(ros::ok()) {
-      ram_msgs::Sim_Temperature temperature_msg;
-      temperature_msg.heat = 23.0;
 
-      temperature_sensor.publish(temperature_msg);
+    /*
+     * Initialize a ROS parameter server param to the default temperature (approx. room temp)
+     */
+    n.setParam("/simulated_temperature_sensor/mean_temperature",DEFAULT_MEAN_TEMP);
 
-      ros::spinOnce();
-      loop_rate.sleep();
-  }
+    /*
+     * Set up a RNG that will be used to add varience to the published temperatures.
+     */
+    double lower_bound = -1;
+    double upper_bound = 1;
+    std::uniform_real_distribution<double> unif(lower_bound,upper_bound);
+    std::default_random_engine re;
+
+    while(ros::ok()) {
+        ram_msgs::Sim_Temperature temperature_msg;
+
+        /*
+         * Retreive the stored mean_temperature from the parameter server. If the parameter
+         * cannot be found, DEFAULT_MEAN_TEMP is used instead.
+         */
+        double mean_temperature;
+        n.param("/simulated_temperature_sensor/mean_temperature",mean_temperature,DEFAULT_MEAN_TEMP);
+
+        /*
+         * The temperature published in the message is the mean_temperature adjusted with
+         * some noise.
+         */
+        temperature_msg.heat = mean_temperature + unif(re);
+
+        temperature_sensor.publish(temperature_msg);
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
+    }
 }
