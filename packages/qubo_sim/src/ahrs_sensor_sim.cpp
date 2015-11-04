104ecb6245e7c24ce6f0dfba95c6681abadb8b00
J-Pai <jesse.pai@gmail.com>
2015-11-03 20:10:18 -0500

    Change class type for message in AHRS file so that it actually gets the right msg type

--- packages/qubo_sim/src/ahrs_sensor_sim.cpp
+++ packages/qubo_sim/src/ahrs_sensor_sim.cpp
@@ -1,8 +1,20 @@
 #include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "ram_msgs/Sim_AHRS.h"
+
+#include <sstream>
+
+void data_callback(const ram_msgs::Sim_AHRS::ConstPtr &msg) {
+    ROS_INFO("I heard: [%f]", msg->orientation.x);
+}
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "simulated_ahrs_sensor");
     ros::NodeHandle n;
 
+    ros::Subscriber ahrs_sub = n.subscribe("/g500/imu", 100, data_callback);
     
+    ros::spin();
+
+    return 0;
 }
