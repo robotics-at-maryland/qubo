1807ba8cce7924b21ddf8199fcf78807c8394b9c
J-Pai <jesse.pai@gmail.com>
2015-11-12 18:29:41 -0500

    basic ahrs sensor setup

--- packages/qubo_sim/src/ahrs_sensor_sim.cpp
+++ packages/qubo_sim/src/ahrs_sensor_sim.cpp
@@ -1,21 +1,28 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "ram_msgs/Sim_AHRS.h"
 #include "sensor_msgs/Imu.h"
 
 #include <sstream>
 
-void data_callback(const sensor_msgs::Imu::ConstPtr &msg) {
-    ROS_INFO("I heard: [%f]", msg->orientation.x);
+sensor_msgs::Imu ahrs_msg;
+ros::Publisher ahrs_pub;
+
+void data_callback(const sensor_msgs::Imu::ConstPtr &imu_msg) {
+    ahrs_msg.orientation.x = imu_msg->orientation.x;
+    ahrs_msg.orientation.y = imu_msg->orientation.y;
+    ahrs_msg.orientation.z = imu_msg->orientation.z;
+
+    ahrs_pub.publish(ahrs_msg);
+    ros::spin();
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "simulated_ahrs_sensor");
     ros::NodeHandle n;
 
+    ahrs_pub = n.advertise<sensor_msgs::Imu>("/qubo/ahrs", 100);
     ros::Subscriber ahrs_sub = n.subscribe("/g500/imu", 100, data_callback);
     
     ros::spin();
-
     return 0;
 }
