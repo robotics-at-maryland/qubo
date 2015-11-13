55293eb8977eccee848b4ec16109475a28ab4337
sgillen <seangillen1887@gmail.com>
2015-11-12 20:38:00 -0500

    adding documentation, mostly just to see how it shows up on doxygen, I don't like how it looks right now but I'll push it anyway.

--- packages/qubo_vehicle_layer/depth_sensor/src/main.cpp
+++ packages/qubo_vehicle_layer/depth_sensor/src/main.cpp
@@ -1,15 +1,29 @@
 #include "depth_sensor_sim.h"
+/**
+This is the main method for our depth_sensor node
+**/
+
 
 int main(int argc, char **argv){
 
-  ros::init(argc, argv, "depth_sensor_node"); //basically always needs to be called first
-  bool simulated = true; //We'll have to pass this one in eventually 
+  ros::init(argc, argv, "depth_sensor_node"); /** basically always needs to be called first */
+  bool simulated = true; /** We'll have to pass this one in eventually */
 
 
-  //if(simulated){
-  DepthSimNode *node = new DepthSimNode(argc, argv, 10); //10 (the rate) is completely arbitary
-  // }
-  
+  /**
+     The basic idea here is to pass in a boolean from a launch script that determines if our class is 
+     a real one or a simulated one. after that they behave exactly the same as far as main is concerned
+     right now though we don't have a real depth sensor, hence the comments
+  **/
+
+
+  // if(simulated){
+    DepthSimNode *node = new DepthSimNode(argc, argv, 10); /** 10 (the rate) is completely arbitrary */
+    //}else{
+    // DepthNode *node = new DepthNode(argc, argv, 10); 
+    // }
+
+
   while (ros::ok()){
     node->update();
     node->publish();
--- packages/qubo_vehicle_layer/include/qubo_node.h
+++ packages/qubo_vehicle_layer/include/qubo_node.h
@@ -6,22 +6,30 @@
 #include "std_msgs/String.h"
 #include <sstream>
 
+ 
+  /*!
+   * \This is the abstract base class that all our vehicle nodes should inherit from  
+   *
+   * We have a publish and an update method that we insist all inherited classes use
+   * We also go ahead and make a node handle and a publisher variable here. 
+   */
 
 class QuboNode {
  public:
-  QuboNode(){}; //Constructor, you should really never call this directly
+ 
+  QuboNode(){}; /**Constructor, you should really never call this directly */
   ~QuboNode(){}; //Destructor 
 
  
   virtual void update() = 0;
   virtual void publish() = 0;
-  //void sendAction(){};  //this isn't a pure function because sub classes won't necessarily use it. 
+  //void sendAction(){};  // this isn't a pure function because sub classes won't necessarily use it.
 
 
   //We'll probably need a few more things 
  protected:
-  ros::NodeHandle n; //the handle for the whole node
-  ros::Publisher publisher; //simulated or real, we'll need a subscriber either way. 
+  ros::NodeHandle n; /** the handle for the whole node */
+  ros::Publisher publisher; /** simulated or real, we'll need a subscriber either way. */
   
 };
 
