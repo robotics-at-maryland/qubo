f7e70541a562ef9385fdceb021dfa97211aa2815
sgillen <seangillen1887@gmail.com>
2015-11-19 19:44:50 -0500

    more documentation

--- packages/qubo_vehicle_layer/depth_sensor/include/depth_sensor_sim.h
+++ packages/qubo_vehicle_layer/depth_sensor/include/depth_sensor_sim.h
@@ -1,3 +1,10 @@
+//! This is the simulated version of our depth sensor
+
+/** This node subscribes to /g500/pressure (though that will need to change when we have our own robot sim up and running) 
+ *  the "pressure" is actually just the depth plus some noise, so we're using it unadulterated. 
+ **/
+
+
 #ifndef DEPTHSIM_HEADER //I don't really see anybody needing to inherit this class, but better safe than sorry. 
 #define DEPTHSIM_HEADER
 
--- packages/qubo_vehicle_layer/include/qubo_node.h
+++ packages/qubo_vehicle_layer/include/qubo_node.h
@@ -1,3 +1,14 @@
+//!  This is the abstract base class that all our vehicle nodes should inherit from
+
+/*! 
+ * The basic idea to have a real and a simulated version of every sensor, BOTH will inherit 
+ * From this class, which means we call both the real and the simulated nodes in exactly the same manner.
+ *
+ * We have a publish and an update method that we insist all inherited classes use
+ * We also go ahead and make a node handle and a publisher variable here. 
+ */
+
+
 #ifndef QUBONODE_HEADER
 #define QUBONODE_HEADER
 
@@ -6,18 +17,11 @@
 #include "std_msgs/String.h"
 #include <sstream>
 
- 
-  /*!
-   * \This is the abstract base class that all our vehicle nodes should inherit from  
-   *
-   * We have a publish and an update method that we insist all inherited classes use
-   * We also go ahead and make a node handle and a publisher variable here. 
-   */
 
 class QuboNode {
  public:
  
-  QuboNode(){}; /**Constructor, you should really never call this directly */
+  QuboNode(){}; /**<Constructor, you should really never call this directly */
   ~QuboNode(){}; //Destructor 
 
  
@@ -28,7 +32,7 @@ class QuboNode {
 
   //We'll probably need a few more things 
  protected:
-  ros::NodeHandle n; /** the handle for the whole node */
+  ros::NodeHandle n; /**< the handle for the whole node */
   ros::Publisher publisher; /** simulated or real, we'll need a subscriber either way. */
   
 };
