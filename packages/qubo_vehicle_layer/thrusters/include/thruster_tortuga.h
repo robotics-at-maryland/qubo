b9e3ae05b5947be0c7b7a484e0e1f19e3e9b2231
Jeremy Weed <jweed262@gmail.com>
2016-03-29 20:00:08 -0400

    working on thruster implementation

--- packages/qubo_vehicle_layer/thrusters/include/thruster_tortuga.h
+++ packages/qubo_vehicle_layer/thrusters/include/thruster_tortuga.h
@@ -5,22 +5,52 @@
 #define THRUSTER_TORTUGA_H
 
 #include "qubo_node.h"
-#include "std_msgs/Float64MultiArray"
+#include "std_msgs/Float64MultiArray.h"
 #include "sensorapi.h"
 
 class ThrusterTortugaNode : QuboNode {
 
 public:
 	ThrusterTortugaNode(int, char**, int);
-	~ThrusterSimNode();
+	~ThrusterTortugaNode();
 
 	void update();
 	void publish();
 	void thrusterCallBack(const std_msgs::Float64MultiArray msg);
 
 protected:
-	std::Float64MultiArray msg;
+
+	/*
+	* Here is the definition for the Tortuga specific Float64MultiArray
+	* used to control the thruster power.
+	*
+	* See: http://docs.ros.org/jade/api/std_msgs/html/msg/Float64MultiArray.html
+	* for more information
+	* 
+	* Every Float64MultiArray includes a "layout" member that specifies
+	* the format of the data in the array.  Here's how our "layout" 
+	* should be set up:
+	* 
+	* dim[0].label = "thruster" 	| descriptor of the data
+	* dim[0].size = 6 				| amount of elements in the array
+	* dim[0].stride = 6				| amount of elements between succsesive 
+	*								|	indexes.  See the website above for
+	*								|	more information
+	* data_offset = 0				| no offset for now
+	*
+	*
+	* multiarray(i,j,k) = data[data_offset + dim_stride[1]*i + dim_stride[2]*j + k]
+	* in our case:
+	*	 multiarry(i) = data[data_offset + i]
+	*
+	* To set thruster 2 to "0.50":
+	* 	data[1] = 0.50
+	*/
+
+
+	std_msgs::Float64MultiArray msg;
 	ros::Subscriber subscriber;
+	int fd;
 
 };
 
--- packages/qubo_vehicle_layer/thrusters/src/thruster_tortuga.cpp
+++ packages/qubo_vehicle_layer/thrusters/src/thruster_tortuga.cpp
@@ -4,19 +4,23 @@
 ThrusterTortugaNode::ThrusterTortugaNode(int argc, char **argv, int rate){
 	ros::Rate loop_rate(rate);
 	subscriber = n.subscribe("/quob/thruster_input", 1000, &ThrusterTortugaNode::thrusterCallBack, this);
+	// copied exactly from lcdshow:
+	fd = openSensorBoard("/dev/ttyUSB0");
 }
 
-ThrusterTortugaNode::~ThrusterTortugaNode(){};
+ThrusterTortugaNode::~ThrusterTortugaNode(){
+	setSpeeds(fd, 0, 0, 0, 0, 0, 0);
+	close(fd);
+}
 
 void ThrusterTortugaNode::update(){
 	ros::spinOnce();
 }
 
 void ThrusterTortugaNode::publish(){
-	//this is where we would put the implementation for intergrating 
-	//the sensor api, but I'm not sure how the float array is set up
+	setSpeeds(fd, msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5]);
 }
 
 void ThrusterTortugaNode::thrusterCallBack(const std_msgs::Float64MultiArray sim_msg){
-	msg.data =sim_msg.data;
+	msg.data = sim_msg.data;
 }
