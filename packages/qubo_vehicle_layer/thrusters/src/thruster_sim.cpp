f056f3ba1e5fd7db39b1ce0410d38ce7674918ef
Edward Nusinovich <edward.nusinovich@gmail.com>
2016-04-12 19:56:41 -0400

    Should properly send Float64MultiArrays

--- packages/qubo_vehicle_layer/thrusters/src/thruster_sim.cpp
+++ packages/qubo_vehicle_layer/thrusters/src/thruster_sim.cpp
@@ -1,4 +1,5 @@
 #include "thruster_sim.h"
+#include "std_msgs/Float64MultiArray.h"
 
 ThrusterSimNode::ThrusterSimNode(int argc, char **argv, int rate){
   ros::Rate  loop_rate(rate);
@@ -9,14 +10,30 @@ ThrusterSimNode::ThrusterSimNode(int argc, char **argv, int rate){
 
 ThrusterSimNode::~ThrusterSimNode(){};
 
+
+  
 /*Turns a R3 cartesian vector into a Float64MultiArray.*/
 void ThrusterSimNode::cartesianToVelocity(double [] velocity){
 
+	/*This is the data that will be contained in the velocity vector */
 	double xPower = velocity[0];
 	double yPower = velocity[1];
 	double zPower = velocity[2];
+	
+	double data [5] = {xPower/2,xPower/2,yPower,zPower/2,zPower/2};
 
-	publisher.publish({xPower/2,xPower/2,yPower,zPower/2,zPower/2});
+	std_msgs::Float64MultiArray msg;
+
+	msg.dim[0].label = "thrust";
+	msg.dim[0].size = 5;
+	msg.dim[0].stride = 5;
+	msg.data[0] = xPower/2;
+	msg.data[1] = xPower/2;
+	msg.data[2] = yPower;
+	msg.data[3] = zPower/2;
+	msg.data[4] = zPower/2;
+	
+	publisher.publish(msg);
 }
 
 void ThrusterSimNode::update(){
