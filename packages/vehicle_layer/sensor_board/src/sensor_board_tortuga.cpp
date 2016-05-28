c99216dbd1f6c390b62413dad6795ffc2de35cb1
sgillen <seangillen1887@gmail.com>
2016-05-28 16:36:53 -0400

    removed check to see if we want the tortuga version of the node, if we got here we already know this is true, no need to check twixe

--- packages/vehicle_layer/sensor_board/src/sensor_board_tortuga.cpp
+++ packages/vehicle_layer/sensor_board/src/sensor_board_tortuga.cpp
@@ -10,15 +10,6 @@ SensorBoardTortugaNode::SensorBoardTortugaNode(int argc, char **argv, int rate):
     checkError(syncBoard(sensor_fd)); // Function from sensorapi.h
     ROS_DEBUG("Synced with the Board");
     
-    if(strcmp(argv[1], "simulated") == 0) {
-        
-    } else if (strcmp(argv[1], "tortuga") == 0) {
-        thrusters.reset(new ThrusterTortugaNode(argc, argv, 10, fd, sensor_file));
-        thrusters.reset(new TempTortugaNode(argc, argv, 10, fd, sensor_file));
-    } else {
-        ROS_ERROR("the pased in arguments to sensor board node (%s) doesn't match anything that makes sense...", argv[1]);
-        exit(1);
-    }
 }
 
 SensorBoardTortugaNode::~SensorBoardTortugaNode() {
