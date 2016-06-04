1acd403dcc624ff66a8c7a11231d4c05b71e86b0
Kevin Wittmer <wittmerkevinr@gmail.com>
2016-06-04 14:08:47 -0400

    Added transformation node for dvl

---
+++ packages/localization/src/trans_dvl.cpp
@@ -0,0 +1,35 @@
+#include <ros/ros.h>
+#include <geometry_msgs/TwistWithCovarianceStamped.h>
+
+ros::Publisher pub;
+geometry_msgs::TwistWithCovarianceStamped msg;
+
+void convertMessage(const underwater_sensor_msgs::DVL underwater_msg) {
+  msg.header.frame_id = "base_link";
+  msg.header.stamp = ros::Time::now();
+        
+  msg.twist.twist.linear.x = underwater_msg.bi_x_axis;
+  msg.twist.twist.linear.y = underwater_msg.bi_y_axis;
+  msg.twist.twist.linear.z = underwater_msg.bi_z_axis;
+        
+  msg.twist.covariance = {1e-9, 0, 0, 0, 0, 0,
+                          0, 1e-9, 0, 0, 0, 0,
+                          0, 0, 1e-9, 0, 0, 0,
+                          0, 0, 0, 1e-9, 0, 0,
+                          0, 0, 0, 0, 1e-9, 0,
+                          0, 0, 0, 0, 0, 1e-9};
+        
+  dvl_data.publish(msg);
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "trans_dvl");
+  ros::NodeHandle n;
+  
+  pub = n.advertise<geometry_msgs::TwistWithCovarianceStamped>("dvl/twist", 1000);
+  
+  ros::Subscriber sub = n.subscribe("qubo/dvl", 1000, convertMessage);
+  ros::spin();
+  
+  return 0;
+}
