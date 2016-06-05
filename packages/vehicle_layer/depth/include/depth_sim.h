adc7c03c4b97c957b55c92ecd1534794c24264a3
sgillen <seangillen1887@gmail.com>
2016-06-05 16:07:57 -0400

    moved depth sensor to new scheme

--- packages/vehicle_layer/depth/include/depth_sim.h
+++ packages/vehicle_layer/depth/include/depth_sim.h
@@ -1,6 +1,3 @@
-//! This is the simulated version of our depth sensor
-
-
 /** Nothing too fancy going on here, we just subscribe to uwsims "pressure" (which uwsim implements by
  *  just taking the depth and adding some noise)
  *  and publishing that to a topic.
@@ -8,29 +5,27 @@
 
 
 
-#ifndef DEPTHSIM_HEADER //I don't really see anybody needing to inherit this class, but better safe than sorry.
+#ifndef DEPTHSIM_HEADER
 #define DEPTHSIM_HEADER
 
-#include "qubo_node.h"
+#include "ram_node.h"
 #include "underwater_sensor_msgs/Pressure.h"
 
-class DepthSimNode : public QuboNode {
-
- public:
-  DepthSimNode(int,char**,int);
-  ~DepthSimNode();
-
-  void update();
-  void publish();
-  void depthCallBack(const underwater_sensor_msgs::Pressure msg);
-
- protected:
-
-  underwater_sensor_msgs::Pressure msg;
-  ros::Subscriber subscriber;
-  ros::Publisher publisher;
-
-
+class DepthSimNode : public RamNode {
+    
+    public:
+    DepthSimNode(std::shared_ptr<ros::NodeHandle> ,int);
+    ~DepthSimNode();
+    
+    void update();
+    void depthCallBack(const underwater_sensor_msgs::Pressure msg);
+    
+    protected:
+    
+    underwater_sensor_msgs::Pressure msg;
+    ros::Subscriber subscriber;
+    ros::Publisher publisher;
+    
 };
 
 
--- packages/vehicle_layer/depth/src/main.cpp
+++ packages/vehicle_layer/depth/src/main.cpp
@@ -2,35 +2,37 @@
 #include "depth_tortuga.h"
 /**
 This is the main method for our depth_sensor node
+The basic idea here is to pass in a boolean from a launch script that determines if our class is 
+a real one or a simulated one. after that they behave exactly the same as far as main is concerned
+right now though we don't have a real depth sensor, hence the comments
 **/
 
 
 int main(int argc, char **argv){
 
-  if(argc != 4){
-    ROS_ERROR("The thruster node received %i arguments which is not right\n", argc);
-    exit(1);
-  }
-  ros::init(argc, argv, "depth_sensor_node"); /** basically always needs to be called first */
+	if(argc != 4){
+		ROS_ERROR("The depth node received %i arguments which is not right\n", argc);
+		exit(1);
+	}
+	
+  std::shared_ptr<ros::NodeHandle> n;
+  ros::init(argc, argv, "depth_sensor_node");
+  
+  
+  std::unique_ptr<RamNode> node;
 
 
-  /**
-     The basic idea here is to pass in a boolean from a launch script that determines if our class is 
-     a real one or a simulated one. after that they behave exactly the same as far as main is concerned
-     right now though we don't have a real depth sensor, hence the comments
-  **/
-
-  std::unique_ptr<QuboNode> node;
   if(strcmp(argv[1], "simulated") == 0){
-    node.reset(new DepthSimNode(argc, argv, 10)); /** 10 (the rate) is completely arbitrary */
+	  node.reset(new DepthSimNode(n, 10)); /** 10 (the rate) is completely arbitrary */
   }else if(strcmp(argv[1], "tortuga") == 0) {
-    node.reset(new DepthTortugaNode(argc, argv, 10, "DEPTH"));
+	  ROS_ERROR("The depth_tortuga  node was tried to launch independently, you want to do it through the sensor board, now exiting");
+	  exit(1);
   }else{
-    ROS_ERROR("the passed in arguments to depth node (%s) doesn't match anything that makes sense..\n", argv[1]);
-    }
-
+	  ROS_ERROR("the passed in arguments to depth node (%s) doesn't match anything that makes sense..\n", argv[1]);
+  }
+  
   while (ros::ok()){
-    node->update();
+	  node->update();
   }
 
 }
