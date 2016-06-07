8a4fbf3f83467dff262e02ca9f3d9d6ce0a56019
sgillen <seangillen1887@gmail.com>
2016-06-07 19:44:33 -0400

    depth is now living the ramnode life

--- packages/vehicle_layer/depth/include/depth_tortuga.h
+++ packages/vehicle_layer/depth/include/depth_tortuga.h
@@ -2,24 +2,23 @@
 #ifndef DEPTHT_HEADER //I don't really see anybody needing to inherit this class, but better safe than sorry.
 #define DEPTHT_HEADER
 
-#include "tortuga_node.h"
+#include "sensor_board_tortuga.h"
 #include "underwater_sensor_msgs/Pressure.h"
 
-class DepthTortugaNode : public TortugaNode {
+class DepthTortugaNode : public SensorBoardTortugaNode {
+	
+    public:
+    DepthTortugaNode(std::shared_ptr<ros::NodeHandle> n, int rate, int fd, std::string file_name);
+    ~DepthTortugaNode();
 
-public:
-  DepthTortugaNode(int, char**, int, std::string);
-  ~DepthTortugaNode();
-
-  void update();
-  void depthCallBack(const underwater_sensor_msgs::Pressure msg);
-
- protected:
-  underwater_sensor_msgs::Pressure msg;
-  ros::Publisher publisher;
-  //int fd; //the file descriptor, established by the a call to openSensorBoard
-
-
+	void update();
+	void depthCallBack(const underwater_sensor_msgs::Pressure msg);
+	
+	protected:
+	underwater_sensor_msgs::Pressure msg;
+	ros::Publisher publisher;
+	//int fd; //the file descriptor, established by the a call to openSensorBoard
+	
 };
 
 #endif
--- packages/vehicle_layer/depth/src/depth_tortuga.cpp
+++ packages/vehicle_layer/depth/src/depth_tortuga.cpp
@@ -2,21 +2,21 @@
 
 
 //we pass argv and argc so in the future we can pass in command line arguments.
-DepthTortugaNode::DepthTortugaNode(int argc, char **argv, int rate, std::string node_name){
-  ros::Rate  loop_rate(rate);
-  publisher = n.advertise< underwater_sensor_msgs::Pressure>("qubo/depth", 1000);
-  //  fd = openSensorBoard("/dev/sensor");
+DepthTortugaNode::DepthTortugaNode(std::shared_ptr<ros::NodeHandle> n,  int rate, int board_fd, std::string board_file):
+	SensorBoardTortugaNode(n, rate, board_fd, board_file){
+	ros::Rate  loop_rate(rate);
+	publisher = n->advertise< underwater_sensor_msgs::Pressure>("qubo/depth", 1000);
 };
 
 DepthTortugaNode::~DepthTortugaNode(){};
 
-
 void DepthTortugaNode::update(){
-  msg.pressure = readDepth(sensor_fd);
-
-  publisher.publish(msg);
+	msg.pressure = readDepth(fd);	
+	publisher.publish(msg);
 }
 
+
+//TODO
 void DepthTortugaNode::depthCallBack(const underwater_sensor_msgs::Pressure sim_msg)
 {
   msg.pressure = sim_msg.pressure;
