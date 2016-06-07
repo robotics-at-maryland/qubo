f0f22392969561eaea4aae685a9b9368683d431f
rb <ross.baehr@gmail.com>
2016-06-07 19:48:58 -0400

    power node converted

--- packages/vehicle_layer/power/include/power_sensor_tortuga.h
+++ packages/vehicle_layer/power/include/power_sensor_tortuga.h
@@ -1,7 +1,8 @@
-#ifndef BATTSIM_HEADER
-#define BATTSIM_HEADER
+#ifndef BATT_TORTUGA_H
+#define BATT_TORTUGA_H
 
-#include "tortuga_node.h"
+#include "sensor_board_tortuga.h"
+#include "sensorapi.h"
 #include "ram_msgs/PowerSource.h"
 
 #define DEFAULT_STATUS false
@@ -9,21 +10,22 @@
 #define DEFAULT_CURRENT 9001.0
 #define DEFAULT_LIFE 3600
 
-class PowerNodeTortuga : public TortugaNode {
+class PowerNodeTortuga : public SensorBoardTortugaNode {
+
+    public:
+      PowerNodeTortuga(std::shared_ptr<ros::NodeHandle>, int rate, int fd ,  std::string file_name);
+      ~PowerNodeTortuga();
+
+      void update();
+
     protected:
   /* Tortuga has 6 Batteries, so this node will have 6 publishers and 6 messages
      instead of the single publisher and message
    */
-        ros::Publisher publisher[6];
-        ram_msgs::PowerSource msg[6];
-
-        struct powerInfo info;
-
-    public:
-        PowerNodeTortuga(int, char **, int, std::string);
-        ~PowerNodeTortuga();
-
-        void update();
+      int fd;
+      ros::Publisher publisher[6];
+      ram_msgs::PowerSource msg;
+      struct powerInfo info;
 };
 
 #endif
