eb5f40f225f0b6a856649c0a02bd837786269f58
rb <ross.baehr@gmail.com>
2016-06-11 18:40:55 -0400

    power topics messed up

--- packages/vehicle_layer/power/src/power_tortuga.cpp
+++ packages/vehicle_layer/power/src/power_tortuga.cpp
@@ -3,7 +3,8 @@
 PowerNodeTortuga::PowerNodeTortuga(std::shared_ptr<ros::NodeHandle> n, int rate, int board_fd, std::string board_file):
   SensorBoardTortugaNode(n, rate, board_fd, board_file) {
     for ( int i = 0; i < 6; i ++ ) {
-      publisher[i] = n->advertise<ram_msgs::PowerSource>(("tortuga/power_source/" + i), 1000);
+      std::string pub_name = "tortuga/power_source" + std::to_string(i);
+      publisher[i] = n->advertise<ram_msgs::PowerSource>(pub_name, 1000);
     }
 }
 
