6c2261eca692e75cafbe6aca09c24760a90aef92
sgillen <seangillen1887@gmail.com>
2016-06-11 15:53:50 -0400

    forgot to initialized the RosNode, done now, I'll need to change all the other nodes since I did this everywhere.. oops!

--- packages/vehicle_layer/sensor_board/src/main.cpp
+++ packages/vehicle_layer/sensor_board/src/main.cpp
@@ -20,19 +20,29 @@
 int main(int argc, char **argv) {
 
     //initialize the ros node we'll use for anything wanting to talk to the sensor board. 
-    std::shared_ptr<ros::NodeHandle> n;
+
+    ROS_ERROR("in main at least...\n");
+    
     ros::init(argc, argv, "sensor_board_node");
+    std::shared_ptr<ros::NodeHandle> n(new ros::NodeHandle);
+    
+
+    ROS_ERROR("made a node\n");
+
 
     //open the sensor board
     std::string sensor_file = "/dev/sensor";
     int fd = openSensorBoard(sensor_file.c_str());
 
-
+    ROS_ERROR("opened the sensor board, fd  =  %i" ,fd );
+   
     //we don't know what type of node we want until we look at the input arguments. 
     std::unique_ptr<SensorBoardTortugaNode> thrusters;
     std::unique_ptr<SensorBoardTortugaNode> depth_sensor;
     std::unique_ptr<SensorBoardTortugaNode> power_sensor;
     std::unique_ptr<SensorBoardTortugaNode> temp_sensor;
+
+    ROS_ERROR("made a pointer to each node we'll want\n");
     
     //SG: add a unique_ptr to your node as well
 
@@ -41,19 +51,28 @@ int main(int argc, char **argv) {
     //SG: This part may have to be a little different for this node, we may want to read from a config file, or just pass in more arguments
     //to specify which sensors are real vs simlated, for now we'll just make sure it works in the case where everything is the real tortuga version
     
-    if(strcmp(argv[1], "simulated") == 0) {
-        //TODO, may actually just want to throw an error and tell the user to launch this shit individually
-    } else if (strcmp(argv[1], "tortuga") == 0) {
-        thrusters.reset(new ThrusterTortugaNode(n, 10, fd, sensor_file));
-        depth_sensor.reset(new DepthTortugaNode(n, 10, fd, sensor_file));
-        power_sensor.reset(new PowerNodeTortuga(n,10,fd,sensor_file));
-        temp_sensor.reset(new TempTortugaNode(n,10,fd,sensor_file));
-        //copy the above with your node, just make sure n, fd and sensor_file are the same, not sure if we need rate honestly and I'd like to remove it if possible
-        
-    } else {
-        ROS_ERROR("the pased in arguments to sensor board node (%s) doesn't match anything that makes sense...", argv[1]);
-        exit(1);
-    }
+
+    //TODO SG: honestly we can put this next bit into a ramnode function, also probably don't want to
+    //be using C-strings for this, segfaults or no fun. I'll hack something today but let's clean this up soon ok?
+
+    //ALSO TODO think of the best way to handlde this, do we really want to pass "tortuga" as an argument if the sensor board is
+    //unique to this robot, we could also honestly abandon the whole passing in argument thing and just launch a different node in the 
+    //launch file... 
+
+    // if(strcmp(string(argv[1])., "simulated") == 0) {
+    //TODO, may actually just want to throw an error and tell the user to launch this shit individually
+    //  } else if (strcmp(argv[1], "tortuga") == 0) {
+    ROS_ERROR("attempting to initialize nodes\n");
+    thrusters.reset(new ThrusterTortugaNode(n, 10, fd, sensor_file));
+    depth_sensor.reset(new DepthTortugaNode(n, 10, fd, sensor_file));
+    power_sensor.reset(new PowerNodeTortuga(n,10,fd,sensor_file));
+    temp_sensor.reset(new TempTortugaNode(n,10,fd,sensor_file));
+    ROS_ERROR("nodes initialized, nice!\n");
+    //copy the above with your node, just make sure n, fd and sensor_file are the same, not sure if we need rate honestly and I'd like to remove it if possible
+    // } else {
+    //   ROS_ERROR("the pased in arguments to sensor board node (%s) doesn't match anything that makes sense...", argv[1]);
+    // exit(1);
+    // }
     
     while (ros::ok()) {
         thrusters->update();
@@ -63,3 +82,4 @@ int main(int argc, char **argv) {
         //make sure you run your nodes update here.
     }
 }
+
