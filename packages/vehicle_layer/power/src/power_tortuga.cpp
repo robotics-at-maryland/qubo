7d350408dde8210da54e3af19c68fdb8be38436c
rb <ross.baehr@gmail.com>
2016-06-11 18:29:35 -0400

    power topics messed up

--- packages/vehicle_layer/power/src/power_tortuga.cpp
+++ packages/vehicle_layer/power/src/power_tortuga.cpp
@@ -10,19 +10,19 @@ PowerNodeTortuga::PowerNodeTortuga(std::shared_ptr<ros::NodeHandle> n, int rate,
 PowerNodeTortuga::~PowerNodeTortuga() {}
 
 void PowerNodeTortuga::update() {
-  checkError(readBatteryVoltages(fd, &info));
-  checkError(readBatteryCurrents(fd, &info));
+	checkError(readBatteryVoltages(fd, &info));
+	checkError(readBatteryCurrents(fd, &info));
 
-  /* This doesn't return an error code, it bitmasks part of readStatus()
-    Not sure which battery this actually is, so for now I'm just setting all them to this value
+	/* This doesn't return an error code, it bitmasks part of readStatus()
+	Not sure which battery this actually is, so for now I'm just setting all them to this value
   */
-  int life = readBatteryUsage(fd);
-  for ( int i = 0; i < 6; i++ ) {
-    msg.voltage = info.battVoltages[i];
-    msg.current = info.battCurrents[i];
-    msg.life = life;
-    publisher[i].publish(msg);
-  }
-  ros::spinOnce();
+	int life = readBatteryUsage(fd);
+	for ( int i = 0; i < 6; i++ ) {
+		msg.voltage = info.battVoltages[i];
+		msg.current = info.battCurrents[i];
+		msg.life = life;
+		publisher[i].publish(msg);
+	}
+	ros::spinOnce();
 
 }
