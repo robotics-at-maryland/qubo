99c83ead2748f1444dcce9402cb768a76c4497e8
sgillen <seangillen1887@gmail.com>
2016-06-14 19:03:54 -0400

    debug messages

--- packages/vehicle_layer/sensor_board/src/thruster_tortuga.cpp
+++ packages/vehicle_layer/sensor_board/src/thruster_tortuga.cpp
@@ -29,24 +29,25 @@ void ThrusterTortugaNode::update(){
     ros::spinOnce();
    
 
-    ROS_DEBUG("Setting thruster speeds");
+    // ROS_DEBUG("Setting thruster speeds");
     int retR = readSpeedResponses(fd);
-    ROS_DEBUG("Read speed before: %x", retR);
+    // ROS_DEBUG("Read speed before: %x", retR);
 
     int retS = setSpeeds(fd, thruster_powers.data[0], thruster_powers.data[1], thruster_powers.data[2], thruster_powers.data[3], thruster_powers.data[4], thruster_powers.data[5]);
-    ROS_DEBUG("Set speed status: %x", retS);
+    //ROS_DEBUG("Set speed status: %x", retS);
     usleep(20*1000);
     int retA = readSpeedResponses(fd);
-    ROS_DEBUG("Read speed after: %x", retA);
+    //ROS_DEBUG("Read speed after: %x", retA);
 
-    ROS_DEBUG("thruster state = %x", readThrusterState(fd));
-    ROS_DEBUG("set speed returns %x", retS);
-    ROS_DEBUG("read speed returns %x", retR);
+    //ROS_DEBUG("thruster state = %x", readThrusterState(fd));
+    // ROS_DEBUG("set speed returns %x", retS);
+    //ROS_DEBUG("read speed returns %x", retR);
    
 }
 
 void ThrusterTortugaNode::thrusterCallBack(const std_msgs::Int64MultiArray new_powers){
   for(int i = 0 ;i < NUM_THRUSTERS; i++){
+      ROS_DEBUG("received a message!");
     thruster_powers.data[i] = new_powers.data[i];
   }
 }
