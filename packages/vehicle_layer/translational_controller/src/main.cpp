ba48d26d9e15685f597c733390fb1d25d4774512
Rajath Shetty <rajathks@outlook.com>
2016-07-09 07:35:20 -0700

    creating two seperate nodes for each controller

---
+++ packages/vehicle_layer/translational_controller/src/main.cpp
@@ -0,0 +1,15 @@
+#include "movement_core.h"
+
+int main(int argc, char **argv) {
+    std::shared_ptr<ros::NodeHandle> n; 
+    ros::init(argc, argv, "move_node");
+    
+    //TODO make work for tortuga node?
+    std::unique_ptr<RamNode> node;
+    node.reset(new moveNode(n, 10));
+    
+    while (ros::ok()){
+        node->update();
+    }
+    
+}
