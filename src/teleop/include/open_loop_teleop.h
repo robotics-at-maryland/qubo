2df7532e135608b722dc242093ef4d9ffe09b047
Kevin Wittmer <wittmerkevinr@gmail.com>
2016-07-19 17:35:54 -0400

    Finished both launch scripts for teleoperation

--- src/teleop/include/open_loop_teleop.h
+++ src/teleop/include/open_loop_teleop.h
@@ -8,19 +8,16 @@
 #define MAX_THRUSTER_INPUT 255
 
 class OpenLoopTeleop {
- 	public:
-  		OpenLoopTeleop(int);
-  		~OpenLoopTeleop();
-		
-		void update();
-
-	private:
-  		ros::NodeHandle nh;
-		std_msgs::Float64MultiArray thruster_input;
-  		ros::Subscriber joy_sub;
-  		ros::Publisher thruster_pub;
-  		void joyInputCallback(const sensor_msgs::Joy::ConstPtr &);
-
+    public:
+        OpenLoopTeleop(int);
+        ~OpenLoopTeleop();		
+        void update();
+    private:
+        ros::NodeHandle nh;
+        std_msgs::Float64MultiArray thruster_input;
+        ros::Subscriber joy_sub;
+        ros::Publisher thruster_pub;
+        void joyInputCallback(const sensor_msgs::Joy::ConstPtr &);
 };
 
 #endif
--- src/teleop/src/open_loop_teleop.cpp
+++ src/teleop/src/open_loop_teleop.cpp
@@ -1,5 +1,10 @@
 #include "open_loop_teleop.h"
 
+/*
+ * Default OpenLoopTeleop constructor. Initializes a subscriber object to subscribe to raw input from the joystick
+ * and call the joyInputCallback callback function. Initializes a publisher object to publish mapped data to the
+ * thrusters. Sets up the thruster input message with the proper dimensions and sets data array to all 0.
+ */
 OpenLoopTeleop::OpenLoopTeleop(int rate) {
     // Subscribe to raw input from the joystick and publish to thruster input
     joy_sub = nh.subscribe<sensor_msgs::Joy>("/joy_input", rate, &OpenLoopTeleop::joyInputCallback, this);
@@ -16,14 +21,24 @@ OpenLoopTeleop::OpenLoopTeleop(int rate) {
     }
 }
 
+/*
+ * Default OpenLoopTeleop destructor.
+ */
 OpenLoopTeleop::~OpenLoopTeleop(){}
 
+/*
+ * Updates the thrusters by publishing the thruster input message to the thruster input topic.
+ */
 void OpenLoopTeleop::update() {
-    /* Publishing messages to topic */
     thruster_pub.publish(thruster_input);
 }
 
-/* Parses the data from the joystick's raw input choosing the inputs that we care about */
+/*
+ * Parses the data from the joystick's raw input and stores it in the thruster input message. Currently only
+ * allows for driving the robot forward/backward and turning it left/right.
+ *
+ * TODO: Add mappings so the robot can dive.
+ */
 void OpenLoopTeleop::joyInputCallback(const sensor_msgs::Joy::ConstPtr &joy_input) {
     // Set the two forward thrusters for now
     // Note: Need to set correct addition/subtraction for joy_input->axes[0]
@@ -31,11 +46,16 @@ void OpenLoopTeleop::joyInputCallback(const sensor_msgs::Joy::ConstPtr &joy_inpu
     thruster_input.data[1] = (-joy_input->axes[1] + joy_input->axes[0]) / 2 * MAX_THRUSTER_INPUT;
 }
 
+/*
+ * Initializes an open loop teleoperation node and publishes thruster input messages at a rate of 10 Hz
+ * while the node is still active.
+ */
 int main(int argc, char **argv) {
     // Initialize the teleop node and node handle
     ros::init(argc, argv, "teleop_node");
     OpenLoopTeleop node(10);
 
+    // Perform callbacks and publish thruster input while node is active
     ros::Rate rate(10);
     while (ros::ok()) {
         ros::spinOnce();
