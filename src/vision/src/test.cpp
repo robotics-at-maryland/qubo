623905cd7ef62ece07e100954130f64b8077cc61
sgillen <sgillen@umd.edu>
2016-09-06 17:30:14 -0400

    building works now, we had two executables called test is all

--- src/vision/src/test.cpp
+++ src/vision/src/test.cpp
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
-
-
 int main(void){
-    CvCapture* capture = cvCaptureFromAVI("infile.avi");
+    CvCapture* capture = cvCaptureFromAVI("~/not_bouy.avi");
     //Capturing a frame:
     printf("wow we didn't crash horribly (well not yet anyway)\n");
-    IplImage* img = 0; 
+    IplImage* img =0; 
     if(!cvGrabFrame(capture)){              // capture a frame 
         printf("Could not grab a frame\n\7");
         exit(0);
