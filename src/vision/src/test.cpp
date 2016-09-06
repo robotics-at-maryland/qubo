1b36b92e7033af939bc34a1144be8e1f80b09982
sgillen <sgillen@umd.edu>
2016-09-06 18:34:03 -0400

    changed over to c++ api, actually read in image now, hardcoded image for now but I'll change it in a second

--- src/vision/src/test.cpp
+++ src/vision/src/test.cpp
@@ -2,15 +2,16 @@
 #include <opencv2/opencv.hpp>
 
 int main(void){
-    CvCapture* capture = cvCaptureFromAVI("~/not_bouy.avi");
+    cv::VideoCapture capture("/home/sgillen/not_bouy.avi");
     //Capturing a frame:
-    printf("wow we didn't crash horribly (well not yet anyway)\n");
-    IplImage* img =0; 
-    if(!cvGrabFrame(capture)){              // capture a frame 
-        printf("Could not grab a frame\n\7");
+    if(!capture.isOpened()){            // capture a frame 
+        printf("couldn't open file/camera\n");
         exit(0);
     }
 
-    img=cvRetrieveFrame(capture);// retrieve the captured frame
+    cv::Mat img;
 
+    capture >> img;
+    std::cout << img;
+    
 }
