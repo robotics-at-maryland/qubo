76518fd955fe047fc50380662e48ef8d540a0d8d
Jeremy Weed <jweed262@gmail.com>
2016-09-22 19:59:59 -0400

    began working on debugging ahrs

--- src/vl_qubo/ahrs/src/main.cpp
+++ src/vl_qubo/ahrs/src/main.cpp
@@ -1,15 +1,12 @@
 #include "ahrs_qubo.h"
 //written by Jeremy Weed
 
-/* Main file for the Qubo AHRS node */
-
-#define AHRS_0_FILE "/currently/unknown"
 
 int main(int argc, char ** argv){
 
 	if (argc != 4){
-		ROS_ERROR("The AHRS node received %i arguments which is not correct\n",
-		argc);
+		ROS_ERROR("The AHRS node received %i arguments which is not correct"
+		" (should be 4)\n", argc);
 		exit(1);
 	}
 
@@ -21,7 +18,7 @@ int main(int argc, char ** argv){
 	std::unique_ptr<RamNode> node0;
 
 	//sets the pointer up
-	node0.reset(new AhrsQuboNode(n, 10, "AHRS_0", AHRS_0_FILE));
+	node0.reset(new AhrsQuboNode(n, 10, "AHRS_0", argv[1]));
 
 	while (ros::ok()){
 		node0->update();
