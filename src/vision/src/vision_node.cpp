fc5f119745010a03c95191799ae2a54e41b3f1a8
sgillen <sgillen@umd.edu>
2016-09-22 19:42:24 -0400

    completed framework added python script to call the dummy service

--- src/vision/src/vision_node.cpp
+++ src/vision/src/vision_node.cpp
@@ -1,5 +1,8 @@
+//sg: this is going to be the primary vision node for qubo (or future robots whatever)
+
 #include "vision_node.h"
 
+//sg: I'm copying the pointer to a node handle scheme we used in the vehicle layer here 
 VisionNode::VisionNode(std::shared_ptr<ros::NodeHandle> n, int rate, std::string feed){
 
     //we'll have to be careful about this, may be best to have a vehicle layer node pass this object to vision, or maybe even just pass the filepath? 
@@ -19,14 +22,15 @@ VisionNode::~VisionNode(){}
 
 void VisionNode::update(){
     cap >> img;
+    ros::spinOnce();
     //std::cout << img;
 }
 
 bool VisionNode::buoy_detector(ram_msgs::bool_bool::Request &req, ram_msgs::bool_bool::Response &res){
     
     // code goes here
-   
-
+    ROS_ERROR("You called the service! nice!");
+    
     return 0;
 
 }
