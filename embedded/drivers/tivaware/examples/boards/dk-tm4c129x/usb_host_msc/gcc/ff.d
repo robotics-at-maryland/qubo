f5299de7cd3ba2e2bc88ec58b8069c55b64aa542
kmonte <lazykylem@gmail.com>
2016-10-14 13:37:46 -0400

    removed unnecessary tivaware dirs, /examples/ /ccs3100-sdk/, /third_party/FreeRTOS, /windows_drivers/

--- embedded/drivers/tivaware/examples/boards/dk-tm4c129x/usb_host_msc/gcc/ff.d
+++
@@ -1,5 +0,0 @@
-gcc/ff.o: ../../../../third_party/fatfs/src/ff.c \
- ../../../../third_party/fatfs/src/ff.h \
- ../../../../third_party/fatfs/src/integer.h \
- ../../../../third_party/fatfs/src/ffconf.h \
- ../../../../third_party/fatfs/src/diskio.h
--- embedded/drivers/tivaware/examples/boards/ek-tm4c129exl/boot_serial/gcc/bl_startup_gcc.d
+++
@@ -1,2 +0,0 @@
-gcc/bl_startup_gcc.o: ../../../../boot_loader/bl_startup_gcc.S \
- ../../../../inc/hw_nvic.h ../../../../inc/hw_sysctl.h bl_config.h
