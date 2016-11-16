3fa6ffa0debb485ba3472a55a7f7d0debf0372cb
kmonte <lazykylem@gmail.com>
2016-11-15 19:57:25 -0500

    added task folders, some basic headers

---
+++ embedded/src/i2c_task/i2c_task.h
@@ -0,0 +1,14 @@
+#include <stdint.h>
+/*
+	Header file for i2c task, includes definitions for addresses and devices
+*/
+
+#ifndef I2C_RAM_H
+#define I2C_RAM_H
+
+/* Everything is one byte long so we should define everything as a uint8_t */
+
+typedef uint8_t address_t;
+typedef uint8_t message_t;
+
+#endif
