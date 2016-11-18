fb8acc37353554e841959f9e0ec0679d1011b6c5
kmonte <lazykylem@gmail.com>
2016-11-17 19:20:28 -0500

    I made some changes apparerently

--- embedded/src/i2c_task/i2c_task.h
+++ embedded/src/i2c_task/i2c_task.h
@@ -3,8 +3,8 @@
 	Header file for i2c task, includes definitions for addresses and devices
 */
 
-#ifndef I2C_RAM_H
-#define I2C_RAM_H
+#ifndef I2C_TASK_H
+#define I2C_TASK_H
 
 /* Everything is one byte long so we should define everything as a uint8_t */
 
