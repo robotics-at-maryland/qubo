fd05b7683a678e7dcb3f524b5184e7e4a933b071
rbaehr <ross.baehr@gmail.com>
2017-01-25 13:08:49 -0800

    organized interrupts into src/interrupts. They must be included and specified in startup_gcc.c

---
+++ embedded/src/interrupts/include/uart_interrupt.h
@@ -0,0 +1,15 @@
+/* Ross Baehr
+   R@M 2017
+   ross.baehr@gmail.com
+*/
+
+#ifndef _UART_INTERRUPT_H_
+#define _UART_INTERRUPT_H_
+
+#include "include/read_uart_queue.h"
+
+QueueHandle_t read_uart_queue;
+
+void UARTIntHandler(void);
+
+#endif
