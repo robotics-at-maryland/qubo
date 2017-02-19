c893fcb571c42e45523c2b14f0b57f100394641a
Jeremy Weed <jweed262@gmail.com>
2017-02-19 01:37:22 -0500

    may have fixed the libraries for the jetson

--- src/vision/src/gpu_vision_node.cpp
+++ src/vision/src/gpu_vision_node.cpp
@@ -2,9 +2,9 @@
 
 GpuVisionNode::GpuVisionNode(std::shared_ptr<ros::NodeHandle> n, std::string feed0, std::string feed1, std::string feedb){
 
-	cv::cuda::GpuMat test;
-	int i = cv::cuda::getCudaEnabledDeviceCount();
-	int e = cv::cuda::getDevice();
+	cv::gpu::GpuMat test;
+	int i = cv::gpu::getCudaEnabledDeviceCount();
+	int e = cv::gpu::getDevice();
 	if(i < 1){
 		ROS_ERROR("There doesn't appear to be a GPU on this system\n");
 		exit(0);
@@ -17,7 +17,7 @@ GpuVisionNode::~GpuVisionNode(){
 }
 
 void GpuVisionNode::update(){
-	cv::cuda::DeviceInfo dev;
+	cv::gpu::DeviceInfo dev;
 	ROS_INFO("%s",dev.name());
 	ROS_INFO("\n");
 	ROS_INFO("%d",dev.majorVersion());
