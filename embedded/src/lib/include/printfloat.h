332bcd5d705e0bf6114962496914b3fdd8351e1d
rbaehr <ross.baehr@gmail.com>
2017-02-21 14:29:30 -0800

    helper lib to printfloat

---
+++ embedded/src/lib/include/printfloat.h
@@ -0,0 +1,12 @@
+#ifndef _PRINTFLOAT_H_
+#define _PRINTFLOAT_H_
+
+#include <math.h>
+
+void reverse(char *str, int len);
+
+int intToStr(int x, char str[], int d);
+
+void ftoa(float n, char *res, int afterpoint);
+
+#endif
