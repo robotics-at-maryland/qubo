0fdfd92f99c53202e0af7b3c711fd58fb18c8832
sgillen <sgillen@umd.edu>
2017-03-21 19:01:15 -0700

    removed sub8 stuff

--- src/command/sub8_missions/sub8/__init__.py
+++
@@ -1,5 +0,0 @@
-# flake8: noqa
-import sub_singleton as sub_singleton
-import pose_editor
-
-from sub_singleton import Searcher
--- src/gnc/.sub8_controller/include/sub8_cvx/cvx.cc
+++
@@ -1,4 +0,0 @@
-#include "solver.h"
-#include "solver.c"
-#include "matrix_support.c"
-#include "ldl.c"
