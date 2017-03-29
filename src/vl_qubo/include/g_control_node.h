1eda3879aabea5813e5bc611f5bf06c7155def7a
sgillen <sgillen@umd.edu>
2017-03-29 16:09:04 -0700

    added angle subscribers

--- src/vl_qubo/include/g_control_node.h
+++ src/vl_qubo/include/g_control_node.h
@@ -5,12 +5,16 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
 #include "nav_msgs/Odometry.h"
+#include "std_msgs/Float64.h"
 
 //c++ library includes
 #include <iostream>
 #include <sstream>
 #include <thread>
 #include <stdio.h>
+
+//tf includes
+#include "tf/tf.h"
 
 class GControlNode{
     public:
@@ -22,7 +26,18 @@ class GControlNode{
     protected:
 
 
-    void orientCallback(const nav_msgs::Odometry::ConstPtr msg);
+    void orientCallback(const nav_msgs::Odometry::ConstPtr& msg);
+    
+
+    
+    void pitchCallback(const std_msgs::Float64::ConstPtr& msg);
+    
+    void yawCallback(const std_msgs::Float64::ConstPtr& msg);
+    
+    void rollCallback(const std_msgs::Float64::ConstPtr& msg);
+    
+
+
 
     std::string _node_name;
 
@@ -32,12 +47,27 @@ class GControlNode{
     //--------------------------------------------------------------------------
     //for now I'm only going to populate the orientation parameters..
        
-    ros::Subscriber _orientSub;
+    ros::Subscriber _orient_sub;
+    ros::Publisher _orient_pub;
     
-    ros::Publisher _orientPub;
+    sensor_msgs::Imu _fused_pose;
+
     
-    sensor_msgs::Imu _fusedPose;
-    
+    //--------------------------------------------------------------------------
+    //thruster variables
+
+    //command subs
+    ros::Subscriber _roll_sub;
+    ros::Subscriber _yaw_sub;
+    ros::Subscriber _pitch_sub;
+
+    //thruster pub
+    std::vector<ros::Publisher> _thruster_pubs;
+
+    //thruster vars
+    double _yaw_command;
+    double _pitch_command;
+    double _roll_command;
 };
 
 #endif
--- src/vl_qubo/src/g_control_node.cpp
+++ src/vl_qubo/src/g_control_node.cpp
@@ -8,48 +8,49 @@ GControlNode::GControlNode(ros::NodeHandle n, string node_name, string fused_pos
 
     string input_pose = "/basic_qubo/pose_gt";
     
-    _orientSub = n.subscribe(input_pose, 1000, &GControlNode::orientCallback, this);
-    _orientPub = n.advertise<sensor_msgs::Imu>(fused_pose_topic.c_str(),1000);
+    _orient_sub = n.subscribe(input_pose, 1000, &GControlNode::orientCallback, this);
+    _orient_pub = n.advertise<sensor_msgs::Imu>(fused_pose_topic.c_str(),1000);
 
 }
 
 GControlNode::~GControlNode(){}
 
 void GControlNode::update(){
-    spinOnce();
-    _orientPub.publish(_fusedPose);
-
+    spinOnce(); //get all the callbacks 
     
-
+    //!!!! sum roll/pitch/yaw into thruster commands here.
+    
+    
 }
 
-void GControlNode::orientCallback(const nav_msgs::Odometry::ConstPtr msg){
+void GControlNode::orientCallback(const nav_msgs::Odometry::ConstPtr &msg){
+
     //may have to convert to quaternions here..
-    ROS_INFO("Seq: [%d]", msg->header.seq);
-    ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
-    ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
-    ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
+    // ROS_INFO("Seq: [%d]", msg->header.seq);
+    // ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
+    //ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+    //ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
+
+
+    //add this to the header? idk
+    //tf::Quaternion ahrsData(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+
     
-    // _fusedPose.header.stamp = ros::Time::now();
-    // _fusedPose.header.seq = ++id;
-	// _fusedPose.header.frame_id = "orientation";
-
-	// _fusedPose.orientation.x = _ahrs_data.quaternion[0];
-	// _fusedPose.orientation.y = _ahrs_data.quaternion[1];
-	// _fusedPose.orientation.z = _ahrs_data.quaternion[2];
-	// _fusedPose.orientation.w = _ahrs_data.quaternion[3];
-	// // the -1's imply we don't know the covariance
-	// _fusedPose.orientation_covariance[0] = -1;
-
-	// _fusedPose.angular_velocity.x = _ahrs_data.gyroX;
-	// _fusedPose.angular_velocity.y = _ahrs_data.gyroY;
-	// _fusedPose.angular_velocity.z = _ahrs_data.gyroZ;
-	// _fusedPose.angular_velocity_covariance[0] = -1;
-
-	// _fusedPose.linear_acceleration.x = _ahrs_data.accelX;
-	// _fusedPose.linear_acceleration.y = _ahrs_data.accelY;
-	// _fusedPose.linear_acceleration.z = _ahrs_data.accelZ;
-	// _fusedPosey.linear_acceleration_covariance[0] = -1;
+    _orient_pub.publish(*msg);
     
-
 }
+
+void GControlNode::pitchCallback(const std_msgs::Float64::ConstPtr& msg){
+    _pitch_command = msg->data;
+}
+
+
+void GControlNode::yawCallback(const std_msgs::Float64::ConstPtr& msg){
+    _yaw_command = msg->data;
+}
+
+
+void GControlNode::rollCallback(const std_msgs::Float64::ConstPtr& msg){
+    _roll_command = msg->data;
+}
+
