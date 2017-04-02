baa4e087447c35bc8e552e3c4fd3d9b8622d8e53
sgillen <sgillen@umd.edu>
2017-04-02 14:17:49 -0700

    changed back to one thread, updated the sleep function to c++ land

--- src/vl_qubo/src/g_main.cpp
+++ src/vl_qubo/src/g_main.cpp
@@ -5,30 +5,35 @@
 
 using namespace std; 
 
-void nodeThread(int argc, char* argv[]){
+// void nodeThread(int argc, char* argv[]){
         
-    ros::init(argc, argv, "control_node");
-    ros::NodeHandle nh;
+//     ros::init(argc, argv, "control_node");
+//     ros::NodeHandle nh;
 
-    GControlNode cn(nh, "control_node", "/qubo/pose/");
+//     GControlNode cn(nh, "control_node", "/qubo/pose/");
 
-    while(1){
-        cn.update();
-        sleep(.5);
-    }
-}
+//     while(1){
+//         cn.update();
+//         sleep(.5);
+//     }
+// }
 
-
+ 
 
 int main(int argc, char* argv[]){
 
-    thread first(nodeThread, argc, argv);
+	//    thread first(nodeThread, argc, argv);
     
-    
+	
+    ros::init(argc, argv, "control_node");
+    ros::NodeHandle nh;
+	
+    GControlNode cn(nh, "control_node", "/qubo/pose/");
+	
     while(1){
-        cout << "hello I'm the main thread" << endl;
-        sleep(1);
+        cn.update();
+        this_thread::sleep_for(chrono::seconds(1));
     }
-    
-    return 0;
+	
+	return 0;
 }
