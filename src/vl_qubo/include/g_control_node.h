602ba9957b103fbab02891a3b57462c7b2b4abef
sgillen <sgillen@umd.edu>
2017-04-13 16:54:46 -0700

    added depth sensor to our hardware node, also  subscribes to a depth topic now and adds that to thruster commands

--- src/vl_qubo/include/g_control_node.h
+++ src/vl_qubo/include/g_control_node.h
@@ -29,23 +29,12 @@ class GControlNode{
 
     void update();
 
-    protected:
 	
-    std::string _node_name;
+    protected:	
+    std::string _node_name; //I'm not totally convinced we need this..
    
-    
     //--------------------------------------------------------------------------
-    //for now I'm only going to populate the orientation parameters..
-       
-    ros::Subscriber _orient_sub;
-	void orientCallback(const sensor_msgs::Imu::ConstPtr& msg);
-	
-    ros::Publisher _orient_pub;
-    
-    sensor_msgs::Imu _fused_pose;
-    
-    //--------------------------------------------------------------------------
-    //thruster variables
+    //command variables
 
     //command subs
     ros::Subscriber _yaw_sub;
@@ -56,14 +45,19 @@ class GControlNode{
 	
     ros::Subscriber _roll_sub;
 	void rollCallback(const std_msgs::Float64::ConstPtr& msg);
-    
+
+	ros::Subscriber _depth_sub;
+	void depthCallBack(const std_msgs::Float64::ConstPtr& msg);
+
+	
     //thruster vars may be able to get rid of these if we store messages..
     double _yaw_command = 0;
     double _pitch_command = 0;
     double _roll_command = 0;
-    
-    
-    //thruster pub
+    double _depth_command = 0;
+
+	
+    //thruster pubs
     std::vector<uuv_gazebo_ros_plugins_msgs::FloatStamped> _thruster_commands;
     std::vector<ros::Publisher> _thruster_pubs;
 
@@ -72,10 +66,21 @@ class GControlNode{
     
     ros::Subscriber _pressure_sub;
 	void pressureCallback(const sensor_msgs::FluidPressure::ConstPtr& msg);
-	double _pressure = 0;
-	
+
+	std_msgs::Float64 _depth; 
 	ros::Publisher  _depth_pub;
 
+	    
+    //--------------------------------------------------------------------------
+    //for now I'm only going to populate the orientation parameters..
+       
+    ros::Subscriber _orient_sub;
+	void orientCallback(const sensor_msgs::Imu::ConstPtr& msg);
+	
+	sensor_msgs::Imu _fused_pose;
+	ros::Publisher _orient_pub;
+    
+	
 };
 
 #endif
--- src/vl_qubo/src/gazebo_hardware_node.cpp
+++ src/vl_qubo/src/gazebo_hardware_node.cpp
@@ -2,7 +2,7 @@
 
 #define WATER_DENSITY 1028.0 //gazebo gets this in the basic_qubo.xacro file in qubo_gazebo
 #define GRAVITY 9.8 //should put both of these into the parameter server probably..
-
+#define SURFACE_PRESSURE 100
 using namespace std;
 using namespace ros;
 
@@ -58,14 +58,12 @@ GControlNode::GControlNode(ros::NodeHandle n, string node_name, string fused_pos
 
 	//this is going to be in meters
 	string depth_topic = cont_namespace + "depth";
-	n.advertise<sensor_msgs::FluidPressure>(depth_topic.c_str(), 1000);
+	_depth_pub = n.advertise<std_msgs::Float64>(depth_topic.c_str(), 1000);
 
 				  
 }
 
 GControlNode::~GControlNode(){}
-
-
 
 /*
 Notes:
@@ -78,66 +76,57 @@ offer up thruster commands directly and add them in?
 
 
 void GControlNode::update(){
-    spinOnce(); //get all the callbacks
-	
-	//!!!! sum roll/pitch/yaw into thruster commands here.
+    spinOnce(); //get all the callbacks, this updates all our x_command variables 
 
+	//reset all our commands to zero
 	for(int i = 0; i < NUM_THRUSTERS; i++){
 		_thruster_commands[i].data = 0;
 		
 	}
 
-
+	
 	//thruster layout found here https://docs.google.com/presentation/d/1mApi5nQUcGGsAsevM-5AlKPS6-FG0kfG9tn8nH2BauY/edit#slide=id.g1d529f9e65_0_3
 	//yaw thruster/surge thrusters
-	
+
+	//add yaw,pitch,roll commands to our thrusters
 	_thruster_commands[0].data += _yaw_command;
 	_thruster_commands[1].data -= _yaw_command;
 	_thruster_commands[2].data -= _yaw_command;
 	_thruster_commands[3].data += _yaw_command;
-
-
+	
 	//pitch/roll thrusters
 	_thruster_commands[4].data += ( _pitch_command + _roll_command);
 	_thruster_commands[5].data += ( _pitch_command - _roll_command);
 	_thruster_commands[6].data += (-_pitch_command - _roll_command);
 	_thruster_commands[7].data += (-_pitch_command + _roll_command);
 
+	
+	//add in depth commands (may have to think about restructurng this?
+	_thruster_commands[4].data += _depth_command;
+	_thruster_commands[5].data += _depth_command;
+	_thruster_commands[6].data += _depth_command;
+	_thruster_commands[7].data += _depth_command;
+	
+
+	
 	for(int i = 0; i < NUM_THRUSTERS; i++){
 		_thruster_pubs[i].publish(_thruster_commands[i]);
 		//	cout << _thruster_commands[i].data << endl;
 	}
 
 
-	//sgillen@20171312-14:13 might want to move to an update function for
-	//every piece of hardware but too lazy to do it rn
-	//_depth = WATER_DENSITY*GRAVITY/_pressure;
-	
-	//TODO put this in a float64(?) message.
-	//have depth update separately, clean up namespace strings (is there another way to do this with arguments to the node? is that worth it?)
-	//clean up other things
-	//test
 	
 }
 
-void GControlNode::orientCallback(const sensor_msgs::Imu::ConstPtr &msg){
-	
-    // ROS_INFO("Seq: [%d]", msg->header.seq);
-    // ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
-    //ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
-    //ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
 
-    //add this to the header?
-
-    //tf::Quaternion ahrsData(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
-	
-    //right now I just pass the data from simulated right through
-	_orient_pub.publish(*msg);
-
-}
+//------------------------------------------------------------------------------
+//callbacks called whenever we get a new actuation command, just stores the data locally
+//we might be more efficient by storing the message (which should stick around as long as we keep
+//a reference to it.
 
 
-//callbacks called whenever we get a new pitch/roll/yaw command, just stores the data locally
+//TODO why do we use a reference to a ConstPtr? I think we just copied some stackoveflow page or ros
+//tutorial, but it doesn't make much sense to me..
 void GControlNode::yawCallback(const std_msgs::Float64::ConstPtr& msg){
     _yaw_command = msg->data;
 }
@@ -150,7 +139,46 @@ void GControlNode::rollCallback(const std_msgs::Float64::ConstPtr& msg){
     _roll_command = msg->data;
 }
 
+void GControlNode::depthCallBack(const std_msgs::Float64::ConstPtr& msg){
+	_depth_command = msg->data; 
+}
+
+
+//--------------------------------------------------------------------------
+// Data callbacks, these take info from the simulator and pass it on in a way
+// that should appear identical to how the real qubo will present it's data
+
+void GControlNode::orientCallback(const sensor_msgs::Imu::ConstPtr &msg){
+	
+    // ROS_INFO("Seq: [%d]", msg->header.seq);
+    // ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
+    //ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+    //ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
+	
+    //right now I just pass the data from simulated right through
+	_orient_pub.publish(*msg);
+
+}
 
 void GControlNode::pressureCallback(const sensor_msgs::FluidPressure::ConstPtr& msg){
-	_pressure = msg->fluid_pressure;
+	
+	//the all caps are constants defined at the beginning of this file
+	//lower case are variables
+	//in our case total_pressure = msg->fluid_pressure
+	//fluid_pressure is in kPa, so we multiply by 1000 to get to pa and therefore a depth of meters
+	
+	//from - https://www.grc.nasa.gov/www/k-12/WindTunnel/Activities/fluid_pressure.html
+	//total_pressure = SURFACE_PRESSURE + water_pressure
+	//fluid_pressure = WATER_DENSITY*GRAVITY*depth
+	//depth = total_pressure - SURFACE_PRESSURE/( WATER_DENSITY*GRAVITY)
+
+	
+	
+	_depth.data = 1000*(msg->fluid_pressure - SURFACE_PRESSURE)/(WATER_DENSITY*GRAVITY);
+	_depth_pub.publish(_depth);
+
+	//have depth update separately, clean up namespace strings (is there another way to do this with arguments to the node? is that worth it?)
+	//clean up other things
+	//test
+	
 }
