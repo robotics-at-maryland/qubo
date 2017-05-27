8a9bd91ebaccc3600cda39973dd8697e75288e4b
sgillen <sgillen@umd.edu>
2017-05-27 13:16:23 -0700

    started work on autonomy node test for the vision action

--- src/autonomy/vision_test_action.py
+++ src/autonomy/vision_test_action.py
@@ -1,13 +1,49 @@
-
 import rospy
 import actionlib
-import VisionExampleAction.msg
+import ram_msgs.msg
+from std_msgs.msg import Float64
 
-client = actionlib.SimpleActionClient("buoy_detect", ram_msgs.msg.ExampleVisionAction)
+# Might make this into a class, doesn't matter too much in this case
 
-goal = ram_msgs.msg.ExampleVisionAction(test_goal = true)
-client.send_goal(goal)
-client.wait_for_result()
-print(client.get_result())
+#globals
+depth = 0
+yaw = 0
 
-buoy_detect(1)
+def launch_node():
+    
+    rospy.init_node('autonomy_node', anonymous=False)
+
+    # client = actionlib.SimpleActionClient("buoy_detect", ram_msgs.msg.ExampleVisionAction)
+
+    # goal = ram_msgs.msg.ExampleVisionAction(test_goal = true)
+    # client.send_goal(goal)
+    # client.wait_for_result()
+    # print(client.get_result())
+    
+    # buoy_detect(1)
+
+    rospy.Subscriber('/qubo/yaw', Float64, yaw_callback)
+    rospy.Subscriber('/qubo/depth', Float64, depth_callback)
+        
+        
+        
+def pretend_vision_action():
+    return (2 - yaw ,2 - depth)
+    
+#there might be some way to make these all one generic function but who cares
+def depth_callback(new_depth):
+    depth = new_depth
+
+def yaw_callback(new_yaw):
+    yaw = new_yaw
+    
+if __name__ == '__main__':
+    launch_node()
+
+    while(True):
+        error_x, error_y = pretend_vision_action()
+
+        
+    
+        
+        
