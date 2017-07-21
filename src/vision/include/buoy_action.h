cc70565750df6c6fec3112145dffc2cb70fcb2f3
Jeremy Weed <jweed262@gmail.com>
2017-07-20 22:15:26 -0400

    vision node compiles, no cv_bridge

--- src/vision/include/buoy_action.h
+++ src/vision/include/buoy_action.h
@@ -2,14 +2,14 @@
 #define BUOY_ACTION_H
 
 
-#include "opencv2/imgcodecs.hpp"
+/* #include "opencv2/imgcodecs.hpp" */
 #include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
+/* #include "opencv2/videoio.hpp" */
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
 #include <opencv2/opencv.hpp>
-#include "opencv2/bgsegm.hpp"
-
+/* #include "opencv2/bgsegm.hpp" */
+#include <opencv2/features2d.hpp>
 //C
 #include <stdio.h>
 
