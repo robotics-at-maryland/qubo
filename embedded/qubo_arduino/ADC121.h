1ce882bfbb39c10f35819002f83058f374346466
Ross Baehr <ross.baehr@gmail.com>
2017-07-25 14:58:58 -0400

    separated hardware drivers into separate files

---
+++ embedded/qubo_arduino/ADC121.h
@@ -0,0 +1,15 @@
+#ifndef _ADC121_H_
+#define _ADC121_H_
+
+#include "Arduino.h"
+
+class ADC121 {
+ public:
+
+  ADC121();
+
+  int getData();
+
+};
+
+#endif
---
+++ embedded/qubo_arduino/PCA9685.h
@@ -0,0 +1,24 @@
+#ifndef _PCA9685_H_
+#define _PCA9685_H_
+
+#include "Arduino.h"
+
+#define BUFFER_SIZE 128
+
+class PCA9685 {
+ public:
+
+  PCA9685();
+
+  void init();
+
+  void thrustersOff();
+  void thrusterSet(uint8_t thruster, uint16_t command);
+
+ private:
+  int freq;
+  uint8_t read8(uint8_t addr);
+  void write8(uint8_t addr, uint8_t d);
+};
+
+#endif
