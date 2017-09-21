ed40405d15048e4292ddee5681817bab8f5d1654
Jeremy Weed <jweed262@gmail.com>
2017-09-21 19:04:44 -0400

    Revert "removed USB interrupt handler"
    This reverts commit 15d41edd5214c8f28267f59f775893012c899d9e.

--- embedded/src/startup_gcc.c
+++ embedded/src/startup_gcc.c
@@ -146,7 +146,7 @@ void (* const g_pfnVectors[])(void) =
     0,                                      // Reserved
     0,                                      // Reserved
     IntDefaultHandler,                      // Hibernate
-    IntDefaultHandler,                   // USB0
+    USB0DeviceIntHandler,                   // USB0
     IntDefaultHandler,                      // PWM Generator 3
     IntDefaultHandler,                      // uDMA Software Transfer
     IntDefaultHandler,                      // uDMA Error
