2ffbf3cf078396dc360d31b152115032501632d6
Jeremy Weed <jweed262@gmail.com>
2017-11-09 19:10:52 -0500

    Mostly able to find the line at comp

--- src/vision/src/tuners/find_lines.py
+++ src/vision/src/tuners/find_lines.py
@@ -16,16 +16,19 @@ else:
     sys.exit(0)
 
 def process_img(frame):
-    red = frame
+    red = frame.copy()
     red[:,:,0] = 0
     # red[:,:,0] = 0
     # return frame
-    gray =cv2.cvtColor(red, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(red, cv2.COLOR_BGR2GRAY)
     # thr = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 4)
-    _, thr = cv2.threshold(red[:,:,2], 100, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    _, thr = cv2.threshold(red[:,:,2], 105, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     # check out opencv find contours
     im2, contours, heirarchy = cv2.findContours(thr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(frame, contours, 1, (0,255,0), 3)
+    c = max(contours, key=cv2.contourArea)
+    eps = 0.005 * cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, eps, True)
+    cv2.drawContours(frame, [approx], -1, (0,255,0), 3)
     return frame
 
 while True:
