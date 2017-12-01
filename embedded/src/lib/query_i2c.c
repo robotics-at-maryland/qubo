a37a3e16689ff1cd854c2cb814e0af31abd5d949
Ross Baehr <ross.baehr@gmail.com>
2017-12-01 16:02:14 -0500

    made non-interrupt based I2C cases atomic. Not sure if necessary but don't think there's any harm

--- embedded/src/lib/query_i2c.c
+++ embedded/src/lib/query_i2c.c
@@ -25,14 +25,22 @@ void writeI2C(uint32_t device, uint8_t addr, uint8_t *data, uint32_t length) {
   ROM_I2CMasterDataPut(device, *data);
 
   if ( length == 1 ) {
+	// So the sceduler doesn't interrupt here
+	taskENTER_CRITICAL();
+
     // Send byte
     ROM_I2CMasterControl(device, I2C_MASTER_CMD_SINGLE_SEND);
 
     // Wait to finish transferring
     while(ROM_I2CMasterBusy(device));
+
+	taskEXIT_CRITICAL();
   }
 
   else if (length == 2) {
+	// So the sceduler doesn't interrupt here
+	taskENTER_CRITICAL();
+
     // Send first word
     ROM_I2CMasterControl(device, I2C_MASTER_CMD_BURST_SEND_START);
     
@@ -44,6 +52,7 @@ void writeI2C(uint32_t device, uint8_t addr, uint8_t *data, uint32_t length) {
 
     while(ROM_I2CMasterBusy(device));
     
+	taskEXIT_CRITICAL();
   }
    
   // This will use the interrupt
