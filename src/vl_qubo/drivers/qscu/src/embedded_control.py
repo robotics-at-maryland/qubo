2010a159303df33427f055dbb4bba3662f50e4c1
Jeremy Weed <jweed262@gmail.com>
2018-03-27 19:50:05 -0400

    fixing stuff on vision lol

--- src/vl_qubo/drivers/qscu/src/embedded_control.py
+++ src/vl_qubo/drivers/qscu/src/embedded_control.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 import rospy
 import cmd
-from std_msgs.msg import Float32
+from std_msgs.msg import Float64
+import sys
 
 qubo_namespace = '/qubo/'
 
@@ -9,22 +10,22 @@ qubo_namespace = '/qubo/'
 class EmbeddedController(cmd.Cmd):
 
     def __init__(self):
-        super(EmbeddedController, self).__init__()
+        cmd.Cmd.__init__(self).__init__()
         rospy.init_node('embedded_controller')
         self.r = rospy.Rate(10)
 
         # publishers and things
-        self.yaw   = rospy.Publisher(qubo_namespace + 'yaw_cmd', Float32,
+        self.yaw   = rospy.Publisher(qubo_namespace + 'yaw_cmd', Float64,
                                      queue_size=5)
-        self.pitch = rospy.Publisher(qubo_namespace + 'pitch_cmd', Float32,
+        self.pitch = rospy.Publisher(qubo_namespace + 'pitch_cmd', Float64,
                                      queue_size=5)
-        self.roll  = rospy.Publisher(qubo_namespace + 'roll_cmd', Float32,
+        self.roll  = rospy.Publisher(qubo_namespace + 'roll_cmd', Float64,
                                      queue_size=5)
-        self.depth = rospy.Publisher(qubo_namespace + 'depth_cmd', Float32,
+        self.depth = rospy.Publisher(qubo_namespace + 'depth_cmd', Float64,
                                      queue_size=5)
-        self.surge = rospy.Publisher(qubo_namespace + 'surge_cmd', Float32,
+        self.surge = rospy.Publisher(qubo_namespace + 'surge_cmd', Float64,
                                      queue_size=5)
-        self.sway  = rospy.Publisher(qubo_namespace + 'sway_cmd', Float32,
+        self.sway  = rospy.Publisher(qubo_namespace + 'sway_cmd', Float64,
                                      queue_size=5)
 
     def do_yaw(self, thrust):
@@ -51,6 +52,11 @@ class EmbeddedController(cmd.Cmd):
         """publish the given [thrust] to the sway topic"""
         self.sway.publish(thrust)
 
+    def do_exit(self, *args):
+        sys.exit()
+
+    def do_quit(self, *args):
+        sys.exit()
 
 if __name__ == "__main__":
     # run the interpreter
